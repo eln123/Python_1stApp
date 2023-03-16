b0ff7ef5d13e462f969b2c42b3f1df063c22c455
Ethan <elnair122@gmail.com>
2023-03-16 14:28:34 -0400

    Python installed in VSCode and on MacOS

---
+++ hello.py
@@ -0,0 +1,17 @@
+# https://code.visualstudio.com/docs/python/python-tutorial#_prerequisites
+# python and VSCode tutorial/docs
+
+str = "hello world"
+
+
+print(str)
+
+# 3 ways to run Python file
+# 1) Right click, then scroll down to Run Python File in terminal
+# 2) Top right corner down arrow, between play and history button, on top of debug in list 
+
+
+# debug
+# go to the hello.py tab
+# go 180 degrees to the right, between teh play button and history button
+# there is a down arrow, click on that to run or debug the file
