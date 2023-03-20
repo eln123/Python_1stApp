74fdecbbbf8c058cb8a44ca35f1ebb5a08139c40
Ethan <elnair122@gmail.com>
2023-03-20 12:48:25 -0400

    very basic stuff for Python

---
+++ python2And3diff.py
@@ -0,0 +1,7 @@
+#Differences between Python2 and Python3
+
+#Python 2 is more structured programming language
+#Python3 is a more Object oriented language
+#Another difference is the "print" function
+# In Python 2, you don't need () round brackets around
+# what you want to print
